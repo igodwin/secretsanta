0cce04419b6bf2963c272fafdeb60fc400bdb8d6
Ivan Godwin <ivan.godwin@gmail.com>
2024-09-16 22:31:21 -0700

    Add queue impl

---
+++ pkg/utils/suite_test.go
@@ -0,0 +1,13 @@
+package utils_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestSecretsanta(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Secretsanta Suite")
+}
