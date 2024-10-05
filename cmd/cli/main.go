f1ca9aa400195168cd78e425818d73e782ff4ab5
Ivan Godwin <ivan.godwin@gmail.com>
2024-10-05 00:32:41 -0700

    Use Logger.log

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,11 +4,47 @@ import (
 	"encoding/json"
 	"fmt"
 	. "github.com/igodwin/secretsanta/internal/participant"
+	"log"
 	"math/rand"
 	"os"
 )
 
 const maxRetries = 1000
+
+func main() {
+	configPath := "participants.json"
+	if envConfigPath := os.Getenv("CONFIG_PATH"); envConfigPath != "" {
+		configPath = envConfigPath
+	}
+	if len(os.Args) > 1 {
+		configPath = os.Args[1]
+	}
+	log.Printf("using config file: %s\n", configPath)
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var participants []*Participant
+	err = json.Unmarshal(data, &participants)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	participants, err = drawNames(participants)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = notify(participants)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
 
 func shuffleParticipants(participants []*Participant) []*Participant {
 	rand.Shuffle(len(participants), func(i, j int) {
@@ -58,39 +94,4 @@ func notify(participants []*Participant) error {
 		fmt.Printf("%s is buying for %s\n", participant.Name, participant.Recipient.Name)
 	}
 	return nil
-}
-
-func main() {
-	configPath := "participants.json"
-	if envConfigPath := os.Getenv("CONFIG_PATH"); envConfigPath != "" {
-		configPath = envConfigPath
-	}
-	if len(os.Args) > 1 {
-		configPath = os.Args[1]
-	}
-	fmt.Printf("Using config file: %s\n", configPath)
-	data, err := os.ReadFile(configPath)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var participants []*Participant
-	err = json.Unmarshal(data, &participants)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	participants, err = drawNames(participants)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	err = notify(participants)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 }
